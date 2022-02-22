979944d0991bd8153e49a0f56c2314588c97ee93
Yevgeniy Shatalin <shatalin_ei@mail.ru>
2022-02-22 21:27:39 +0600

    feat: lesson 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,25 @@
 import React from 'react';
 import './App.css';
+import {Rating} from "./components/Rating/Rating";
+import {Accordion} from "./components/Accordion/Accordion";
 
 function App() {
     return (
         <div className="App">
-            <Rating/>
-            <Accordion/>
+            <PageTitle title={'This is APP component'}/>
+            <Rating value={4}/>
+            <Accordion titleValue={'Menu'} collapsed={false}/>
+            <Accordion titleValue={'Pages'} collapsed={true}/>
         </div>
     );
 }
 
-function Rating() {
-    return (
-        <div className="rating">
-            <h2>Rating</h2>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-        </div>
-    );
+type PageTitlePropsType = {
+    title: string
 }
 
-function Accordion() {
-    return (
-        <div>
-            <AccordionTitle/>
-            <AccordionBody/>
-        </div>
-    )
-        ;
-}
-
-function Star() {
-    return (
-        <div className="star">
-
-        </div>
-    );
-}
-
-function AccordionTitle() {
-    return (
-        <h3>Menu</h3>
-    );
-}
-
-function AccordionBody() {
-    return (
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-        </ul>
-    );
+function PageTitle(props: PageTitlePropsType) {
+    return <h1>{props.title}</h1>
 }
 
 export default App;
